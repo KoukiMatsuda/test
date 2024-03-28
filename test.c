55efaff746e1dc34ec000d762ab21a85cbb0c99b
k.matsuda <k.matsuda@nskint.co.jp>
2024-03-28 14:42:49 +0900

    test

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    signed char cData1 = 0x01;
+    signed char cData2 = 0x0A;
+    signed char cData3;
+    signed char cData4;
+
+    cData1 =+ 0x03;
+    printf("cData1 = 0x%x\n", cData1);
+    cData2 += cData1;
+
+    cData3 = cData2++;
+    cData4 = --cData1;
+
+    printf("cData1 = 0x%x\n", cData1);
+    printf("cData2 = 0x%x\n", cData2);
+    printf("cData3 = 0x%x\n", cData3);
+    printf("cData4 = 0x%x\n", cData4);
+
+    return 0;
+}
